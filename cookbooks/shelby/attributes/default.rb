9f1916713f1b6af695330f60f4e956a38f8b3170
Joshua Samberg <josh@shelby.tv>
2012-10-26 17:22:16 -0400

    Explicitly use p194 patch level of Ruby 1.9.3

--- cookbooks/shelby/attributes/default.rb
+++ cookbooks/shelby/attributes/default.rb
@@ -1,8 +1,8 @@
 default['shelby']['user']['name'] = 'gt'
 
 default['shelby']['user_rvm']['username'] = 'gt'
-default['shelby']['user_rvm']['default_ruby'] = 'ruby-1.9.3'
-default['shelby']['user_rvm']['rubies'] = ['ruby-1.9.3']
+default['shelby']['user_rvm']['default_ruby'] = 'ruby-1.9.3-p194'
+default['shelby']['user_rvm']['rubies'] = ['ruby-1.9.3-p194']
 
 default['shelby']['web']['domain'] = 'shelby.tv'
 default['shelby']['web']['certificates']['dir'] = "#{node['nginx']['dir']}/certificates"
