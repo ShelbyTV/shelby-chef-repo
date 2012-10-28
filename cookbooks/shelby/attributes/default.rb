4fd56f63a050dc99fdab5e3cea766c2623e7b1d8
Joshua Samberg <josh@shelby.tv>
2012-10-28 14:50:14 -0400

    Specify rvm version that works for us

--- cookbooks/shelby/attributes/default.rb
+++ cookbooks/shelby/attributes/default.rb
@@ -3,6 +3,8 @@ default['shelby']['user']['name'] = 'gt'
 default['shelby']['user_rvm']['username'] = 'gt'
 default['shelby']['user_rvm']['default_ruby'] = 'ruby-1.9.3-p194'
 default['shelby']['user_rvm']['rubies'] = ['ruby-1.9.3-p194']
+default['shelby']['user_rvm']['version'] = '1.16.14'
+default['shelby']['user_rvm']['branch'] = 'none'
 
 default['shelby']['web']['domain'] = 'shelby.tv'
 default['shelby']['web']['certificates']['dir'] = "#{node['nginx']['dir']}/certificates"
