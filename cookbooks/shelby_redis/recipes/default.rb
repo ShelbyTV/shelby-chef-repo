1a920f7353a27e844c7b829366e82eb80501f4b7
Joshua Samberg <josh@shelby.tv>
2012-11-12 14:18:48 -0500

    Added lightweight cookbook for specifying redis attributes and installing redis

---
+++ cookbooks/shelby_redis/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: shelby_redis
+# Recipe:: default
+#
+# Copyright 2012, Shelby.tv
+#
+# All rights reserved - Do Not Redistribute
+#
+# Install a redis server
+
+include_recipe "redis"
